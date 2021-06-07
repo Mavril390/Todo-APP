973c66fec7fd7f587b68185cd292e545c21a2b64
Mauricio Marinkovich <maumarinkovich@gmail.com>
2021-06-07 01:30:35 -0300

    Primer commit donde se sube la pagina

---
+++ src/components/TodoList.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Todo from './Todo'
+
+function TodoList({ todos, setTodos, filteredTodos }){
+    return(
+        <div className="todo-container">
+            <ul className="todo-list">
+                {filteredTodos.map(todo => (
+                    <Todo todo={todo} todos={todos} setTodos={setTodos} text={todo.text} key={todo.id}/>
+                ))}
+            </ul>
+        </div>
+    )
+}
+
+export default TodoList;
