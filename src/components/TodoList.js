0248456392f93dc61e919f2daa3e9447cbb6db6c
Mauricio Marinkovich <maumarinkovich@gmail.com>
2021-06-07 20:56:30 -0300

    Se agrega codigo para que sea mas responsive

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,7 +3,7 @@ import Todo from './Todo'
 
 function TodoList({ todos, setTodos, filteredTodos }){
     return(
-        <div className="todo-container">
+        <div className="todo-container container">
             <ul className="todo-list">
                 {filteredTodos.map(todo => (
                     <Todo todo={todo} todos={todos} setTodos={setTodos} text={todo.text} key={todo.id}/>
